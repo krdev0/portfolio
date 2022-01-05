22560a28d8c0acf76d0e567853eea3018b1e7a3f
Kristaps Ritins <kritinsdev@gmail.com>
2022-01-06 01:05:58 +0200

    finished header + started hero block

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+          Hi, Im Kristaps <br/>
+          Full Stack Developer  
+      </SectionTitle>
+      <SectionText>
+        I am a developer with 4 years of experience developing websites and web applications using different languages and technologies.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
