3a263c744791a2d84d677f2f29d9f63a7ef2c60f
Kristaps Ritins <kritinsdev@gmail.com>
2022-02-18 18:20:46 +0200

    file structure done

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { About, Footer, Header, Skills, Testimonial, Work } from './container/index';
+import { Navbar } from './components';
+
 export const App = () => {
-  return (
-    <h1>App</h1>
-  );
+    return (
+        <div className='app'>
+            <Navbar />
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
