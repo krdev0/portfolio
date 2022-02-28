b97be26abba287456531ca9b09d6c0b69895caaa
Kristaps Ritins <kritinsdev@gmail.com>
2022-02-28 23:01:38 +0200

    started work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setactiveFilter] = useState('All');
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 })
+  const handleWorkFilter = (item) => {
+
+  }
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span></h2>
+      <div className='app__work-filter'>
+        {['UI/UX', "Web App", 'Mobile App', 'React JS', 'All'].map((item, index) => {
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        })}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+      </motion.div>
+    </>
   )
 }
 
-export default Work
+export default AppWrap(Work, 'about');
